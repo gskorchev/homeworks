e98c84067c8dec8d656d89903cfdb438d164ae39
gskorchev <gskorchev@gmail.com>
2018-11-02 15:27:38 +0200

    updated

---
+++ javascript/strings.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const firstName = 'Pesho',
+      middleName = "Goshov",
+      lastName = `Stamatov`;
+const fullname = firstName + ' ' + middleName + ' ' + lastName;
+
+console.log(fullname);
+console.log(`${firstName} ${middleName} ${lastName}`);
--- javascript/variables.js
+++ javascript/variables.js
@@ -1,15 +1,30 @@
-'use strict';
-
-let name = 'Pesho';
-console.log(name);
-
-name = false;
-console.log(name);
-
-let age = 17;
-age += 1;
-console.log(age);
-
-const interger = '12345.6789';
-
-console.log(parseFloat(interger) + 10);
+// 'use strict';
+//
+// let name = 'Pesho';
+// console.log(name);
+//
+// name = false;
+// console.log(name);
+//
+// let age = 17;
+// age += 1;
+// console.log(age);
+//
+// const interger = '12345.6789Gosho';
+//
+// console.log(parseFloat(interger) + 10);
+//
+// const a = 0.2;
+// const b = 0.1;
+// const sum = 0.3;
+//
+// const MAX_DIFF = 0.0001;
+// console.log(Math.abs((a + b) - sum) < MAX_DIFF);
+//
+// console.log(
+//   !!'',
+//   !!'gosho',
+//   !!0,
+//   !!15
+// );
+// //
