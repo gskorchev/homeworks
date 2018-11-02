29b8653fd31261fefd7fb4f424359a9906b778d6
gskorchev <gskorchev@gmail.com>
2018-11-02 14:07:40 +0200

    lessons updated

---
+++ javascript/variables.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let name = 'Pesho';
+console.log(name);
+
+name = false;
+console.log(name);
+
+let age = 17;
+age += 1;
+console.log(age);
+
+const interger = '12345.6789';
+
+console.log(parseFloat(interger) + 10);
